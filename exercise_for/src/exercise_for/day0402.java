365b61da5482fec67b83cd7499a6395f34ade75a
spl <1274471576@qq.com>
2016-04-08 22:32:59 +0800

    java_for

---
+++ exercise_for/src/exercise_for/day0402.java
@@ -0,0 +1,46 @@
+package exercise_for;
+
+import java.util.Scanner;
+
+public class day0402 {
+	public static void main(String[] args)
+	{
+		/*int i, sum;
+		sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("输入结束数字：");
+		int n = sc.nextInt();
+		for(i=1; i<=n; i++)
+		{
+			//sum=sum+i;
+			sum+=i;
+		}
+		System.out.println("累加和为："+sum);
+		sc.close();*/
+		
+		
+		/*int a;
+		double b=3;double c;
+		a=10;
+		c = a/b;
+		System.out.println("10÷3="+c);8*/
+		
+		/*int a,b,c,d;
+		a = 6 + 7;
+		System.out.println("a = "+a);
+		b = a % 5;
+		System.out.println("b = "+b);
+		c = b++;
+		System.out.println("b++后  b ="+b);
+		System.out.println("c = "+c);
+		d = ++b;
+		System.out.println("++b后 b ="+b);
+		System.out.println("d = "+d);*/
+		
+		int a = 1, b=2;
+		int c = a&b;
+		System.out.println("c = "+c);
+		
+		
+	}
+}
