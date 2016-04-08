29347eaf2e546c302ec7d8fcc6b0842190bc6680
spl <1274471576@qq.com>
2016-04-08 22:34:13 +0800

    java_HelloWorld

---
+++ helloworld/src/helloworld/helloworld.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		/*double x;
+		x=-3.14;
+		if (x>0)
+		{
+		}
+		else
+		{
+			x=-x;
+		}
+		System.out.println("x的绝对值是："+x);*/
+	}
+}
