338be914e2e1c685866b0905d1c023685572386e
spl <1274471576@qq.com>
2016-04-09 10:27:08 +0800

    多态父类添加

---
+++ sylclass10/src/sylclass10/Telephone.java
@@ -0,0 +1,6 @@
+package sylclass10;
+
+public abstract class Telephone {
+	public abstract void call();
+	public abstract void message();	
+}
---
+++ sylclass10/src/sylclass10/test.java
@@ -0,0 +1,7 @@
+package sylclass10;
+
+public class test {
+	public static void main(String[] args){
+		CellPhone a = new CellPhone();
+	}
+}
